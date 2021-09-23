e0f2b9e1d85f228c939cdcde70314590521106a5
kbkk <6276426+kbkk@users.noreply.github.com>
2021-09-24 00:28:08 +0200

    chore: add start/finish time log to backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+echo "Backup started at $(date)"
+
 BACKUP_FILENAME=backup.tar.gz
 
 ./b2 authorize-account "$B2_KEY_ID" "$B2_APP_KEY"
@@ -9,3 +11,5 @@ BACKUP_FILENAME=backup.tar.gz
 tar -zcf /tmp/"$BACKUP_FILENAME" /var/backups
 
 ./b2 upload-file "$B2_BUCKET" /tmp/"$BACKUP_FILENAME" "$BACKUP_FILENAME"
+
+echo "Backup finished at $(date)"
